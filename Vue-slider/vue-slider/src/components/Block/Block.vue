b899217173b7a49a3d0503024d6df522ac6ec3d5
dominik <dominik@dominikcichon.de>
2022-10-26 16:57:47 +0200

    Add resize observer and basic styles

--- Vue-slider/vue-slider/src/components/Block/Block.vue
+++ Vue-slider/vue-slider/src/components/Block/Block.vue
@@ -2,10 +2,16 @@
 <style src="./Block.scss" lang="scss"></style>
 
 <script>
+import { useMedia } from '../../composables/useMedia'
 export default {
   props: {
     layout: String,
     title: String
+  },
+  data () {
+    return {
+      showSlider: useMedia('(max-width: 768px)')
+    }
   }
 }
 </script>
